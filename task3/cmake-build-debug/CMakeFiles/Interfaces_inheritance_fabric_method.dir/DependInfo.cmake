b4b80d553f923be65301b4182837386ee3b49a0a
Roman <romaderunec@gmail.com>
2021-12-31 14:19:11 +0000

    Add files via upload

---
+++ task3/cmake-build-debug/CMakeFiles/Interfaces_inheritance_fabric_method.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roman/Downloads/task3/dump.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/dump.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/dump.cpp.o.d"
+  "/home/roman/Downloads/task3/factory.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/factory.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/factory.cpp.o.d"
+  "/home/roman/Downloads/task3/grep.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/grep.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/grep.cpp.o.d"
+  "/home/roman/Downloads/task3/main.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/main.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/main.cpp.o.d"
+  "/home/roman/Downloads/task3/parser.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/parser.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/parser.cpp.o.d"
+  "/home/roman/Downloads/task3/readfile.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/readfile.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/readfile.cpp.o.d"
+  "/home/roman/Downloads/task3/replace.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/replace.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/replace.cpp.o.d"
+  "/home/roman/Downloads/task3/sort.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/sort.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/sort.cpp.o.d"
+  "/home/roman/Downloads/task3/worker.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/worker.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/worker.cpp.o.d"
+  "/home/roman/Downloads/task3/writefile.cpp" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/writefile.cpp.o" "gcc" "CMakeFiles/Interfaces_inheritance_fabric_method.dir/writefile.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task3/cmake-build-debug/CMakeFiles/Interfaces_inheritance_fabric_method.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/dump.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/dump.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/factory.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/factory.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/grep.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/grep.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/main.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/main.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/parser.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/parser.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/readfile.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/readfile.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/replace.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/replace.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/sort.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/sort.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/worker.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/worker.cpp.o.d"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/writefile.cpp.o"
+  "CMakeFiles/Interfaces_inheritance_fabric_method.dir/writefile.cpp.o.d"
+  "Interfaces_inheritance_fabric_method"
+  "Interfaces_inheritance_fabric_method.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Interfaces_inheritance_fabric_method.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
